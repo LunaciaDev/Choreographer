409fac5d2d48911ce42f0fdac39f2fb953385724
Lunacia <130038743+LunaciaDev@users.noreply.github.com>
2025-02-13 12:43:47 +0000

    feat: preprocess result for UI

--- core/src/main/java/com/lunaciadev/choreographer/core/ResultGenerator.java
+++ core/src/main/java/com/lunaciadev/choreographer/core/ResultGenerator.java
@@ -1,5 +1,6 @@
 package com.lunaciadev.choreographer.core;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 import com.lunaciadev.choreographer.data.ItemData;
@@ -7,6 +8,7 @@ import com.lunaciadev.choreographer.types.Crate;
 
 public class ResultGenerator {
     private HashMap<Integer, Crate> result;
+    private Crate[] sortedResult;
     private ItemData itemData;
 
     public ResultGenerator(ItemData itemData) {
@@ -14,7 +16,20 @@ public class ResultGenerator {
     }
 
     public void getResult(HashMap<Integer, Crate> result) {
+        Integer[] sortedKeys = new Integer[result.size()];
+        sortedResult = new Crate[result.size()];
+
         this.result = result;
+        result.keySet().toArray(sortedKeys);
+        Arrays.sort(sortedKeys);
+
+        for (int i = 0; i < result.size(); i++) {
+            sortedResult[i] = result.get(sortedKeys[i]);
+        }
+    }
+
+    public Crate[] getSortedResult() {
+        return sortedResult;
     }
 
     public String generateGoogleSheetPaste() {
